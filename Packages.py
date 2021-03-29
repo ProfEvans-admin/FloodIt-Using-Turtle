34402ba463a4b9bc6e634ac7330c6fe606cdef8a
ProfEvans-admin <74271379+ProfEvans-admin@users.noreply.github.com>
2021-03-29 12:04:50 -0500

    Add files via upload

---
+++ Packages.py
@@ -0,0 +1,11 @@
+import pip
+
+def install(package_name):
+        try:
+            pip.main(['install', package_name])
+        except:
+            print("Unable to install " + package_name)
+
+imports = ['pillow','pyscreenshot', 'pyautogui']
+for i in imports:
+    install(i)
